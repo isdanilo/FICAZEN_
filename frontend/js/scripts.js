148e5fcebf61713dc7822c1bed65495cc9af07b7
Alunos <alunos@pedreira-local.org>
2024-11-19 18:56:42 -0300

    Normalize line endings

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -84,7 +84,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://ficazen.onrender.com")
     websocket.onmessage = processMessage
     
 
